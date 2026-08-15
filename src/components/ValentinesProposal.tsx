667da1a46a8e4cea95b1bbe84d9e60b918822198
wailnou00-hub <wailnou00@gmail.com>
2026-08-15 21:18:09 +0100

    Update ValentinesProposal.tsx

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -9,7 +9,7 @@ const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
 });
 
-// 36 images
+// 12 images
 const images = [
   "/game-photos/1.avif",
   "/game-photos/2.avif",
@@ -23,30 +23,7 @@ const images = [
   "/game-photos/10.avif",
   "/game-photos/11.avif",
   "/game-photos/12.avif",
-  "/game-photos/13.avif",
-  "/game-photos/14.avif",
-  "/game-photos/15.avif",
-  "/game-photos/16.avif",
-  "/game-photos/17.avif",
-  "/game-photos/18.avif",
-  "/game-photos/19.avif",
-  "/game-photos/20.avif",
-  "/game-photos/21.avif",
-  "/game-photos/22.avif",
-  "/game-photos/23.avif",
-  "/game-photos/24.avif",
-  "/game-photos/25.avif",
-  "/game-photos/26.avif",
-  "/game-photos/27.avif",
-  "/game-photos/28.avif",
-  "/game-photos/29.avif",
-  "/game-photos/30.avif",
-  "/game-photos/31.avif",
-  "/game-photos/32.avif",
-  "/game-photos/33.avif",
-  "/game-photos/34.avif",
-  "/game-photos/35.avif",
-  "/game-photos/36.avif",
+  
 ];
 
 export default function ValentinesProposal() {
